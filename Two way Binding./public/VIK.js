a891b2cbb5faa9cdafe5d3813e5e9c736af1c787
Vikram Kumar <vikramkumar@Vikrams-MacBook-Air.local>
2022-10-24 16:46:48 +0530

    Successfully Added Two Way Binding

---
+++ Two way Binding./public/VIK.js
@@ -0,0 +1,15 @@
+const model = new Vue({
+    el: '#vik1',
+    data: {
+        first: " "
+    }
+
+})
+const model1 = new Vue({
+    el: '#vik2',
+    data: {
+        last: " "
+    }
+
+})
+
---
+++ Two way Binding./src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+
+
+
+
+</template>
+
+<script    >
+export default {
+ 
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.box {
+    border: 5px dotted green;
+    text-align: center;
+    background-color: azure;
+
+}
+.H1{
+
+    font-size: 50px;
+    font-style: italic;
+    color: blue;
+}
+.H2{
+
+    font-size: 35px;
+    font-style: oblique;
+    color: aqua;
+}
+.H3{
+
+    font-size: 20px
+}
+.H4{
+
+    font-size: 20px
+}
+
+
+</style>
